40716501baf933da70adb9dd06d895d4a9c7f039
Mohamed-Rebbouh <121132111+Mohamed-Rebbouh@users.noreply.github.com>
2024-03-16 02:42:16 +0100

    challenge2/task1

--- Challenge2/task1/utils.py
+++ Challenge2/task1/utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 import re
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.tokenize import word_tokenize
